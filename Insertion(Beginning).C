c423658543ccd307a9137481b58473ccf01a3709
T-Dharini <dharini24102004@gmail.com>
2025-03-20 15:34:18 +0530

    Update Insertion(Beginning).C

--- Insertion(Beginning).C
+++ Insertion(Beginning).C
@@ -1,1 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i, element;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n+1]; 
+    printf("Enter %d elements of the array: \n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to insert at the beginning: ");
+    scanf("%d", &element);
+    for (i = n - 1; i >= 0; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[0] = element;
+    printf("Array after insertion: \n");
+    for (i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
 
